f55a0ca89147a9b140a85c647239e30e20d0ab77
Chandrashekhar Patil <balajibalajipatil4@gmail.com>
2026-03-01 10:31:16 +0530

    Register Page User Interface Made Successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,5 +6,7 @@ const API = axios.create({
 
 // Login API
 export const loginUser = (formData) => API.post("/auth/login", formData);
+// Register 
+export const registerUser = (data) => API.post("/auth/register", data);
 
 export default API;
